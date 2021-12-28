4dc1a5b737400cdaded6c0ae1cc68f0c92fbb0a1
lachiejames <lachie_james@hotmail.com.au>
2021-12-29 00:35:46 +1100

    add some modules

---
+++ conversation_assistant/parsers.py
@@ -0,0 +1,14 @@
+from openai import Completion
+from conversation_assistant.models.gpt3_completion_response import (
+    GPT3CompletionResponse,
+)
+
+
+def fetch_completetion(prompt: str) -> GPT3CompletionResponse:
+    return Completion.create(
+        prompt=prompt,
+        engine="davinci-instruct-beta-v3",
+        temperature=0.7,
+        n=3,
+        max_tokens=50,
+    )
