8ba06a8c0f0555b6744548cb4bea358e1ad9b714
lachiejames <lachie_james@hotmail.com.au>
2021-12-29 01:07:35 +1100

    fix: make pipeline pass

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 ROOT_DIRECTORY = Path(__file__).parent
-__version__ = "0.3.0"
+__version__ = "0.0.1"
 
 
 setup(
     name="conversation_assistant",
     version=__version__,
-    description="Suggests messages to respond with based on your conversation history",
+    description="Suggests messages for a user to respond with based upon their conversation history",
     url="https://pypi.org/project/conversation_assistant/",
     packages=find_packages(),
 )
