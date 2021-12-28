c3f88a1e3a22beba29c2f81b0e494b70e09a63a8
lachiejames <lachie_james@hotmail.com.au>
2021-12-28 17:04:15 +1100

    refactor

--- src/main.py
+++ src/main.py
@@ -1,30 +1,43 @@
 import os
 
 import openai
-from openai.api_resources.completion import Completion
+from openai import Completion
 
-PROMPT = "mine was good.  I just had a lazy day today\n"
-ENGINE = "davinci-instruct-beta"
+PROMPT = """
+Ideas involving education and virtual reality
+
+1. Virtual Mars
+Students get to explore Mars via virtual reality and go on missions to collect and catalog what they see.
+"""
+PROMPTS = [
+    "Hello, how are you today?",
+]
+ENGINE = "davinci-instruct-beta-v3"
 
 
 def set_api_key() -> None:
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def ask_question(question: str) -> str:
-    response = openai.Completion.create(
-        prompt=question,
-        engine=ENGINE,
-        temperature=0,
-        top_p=1,
+def get_responses_to_prompts(prompts: list[str]) -> list[str]:
+    response = Completion.create(
+        prompt=prompts, engine=ENGINE, temperature=0.7, n=3, max_tokens=50
     )
 
-    return response.choices[0].text
+    list_of_responses = [choice.text for choice in response.choices]
+    return list_of_responses
+
+
+def log_responses(responses: list[str]):
+    for response in responses:
+        print(f"\n\n\nResponse - {response}")
 
 
 def run():
-    print(f"Question - {PROMPT}")
+    print(f"Question - {PROMPTS}")
 
-    for i in range(10):
-        response = ask_question(PROMPT)
-        print(f"\n\n\n\n\n\n\n\n\n\n\n\nResponse #{i} - {response}")
+    choices = get_responses_to_prompts(PROMPTS)
+    log_responses(choices)
+
+
+run()
