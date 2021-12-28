b0675f22b42d03dad98d4e0d809e6ede7fa16a06
lachiejames <lachie_james@hotmail.com.au>
2021-12-28 18:41:52 +1100

    add first test

---
+++ conversation_assistant/test/mock_chatlog.py
@@ -0,0 +1,5 @@
+MOCK_CHATLOG = """
+    Human - Hello, how are you today?,
+    Bot - Hi I am good.  I am 45, how old are you?,
+    Human - I am 30, what do you do for work?,
+"""
---
+++ conversation_assistant/test/test_main.py
@@ -0,0 +1,34 @@
+from openai import Completion
+
+from conversation_assistant.test.mock_chatlog import MOCK_CHATLOG
+
+
+def extract_suggestions(choices_response: list) -> list[str]:
+    suggestions: list[str] = []
+
+    for choice in choices_response:
+        suggestions.append(choice.text.lstrip())
+
+    return suggestions
+
+
+def get_suggestions_for_next_message(chatlog: str) -> list[str]:
+    gpt3_completion_response = Completion.create(
+        prompt=chatlog,
+        engine="davinci-instruct-beta-v3",
+        temperature=0.7,
+        n=3,
+        max_tokens=50,
+    )
+
+    return extract_suggestions(gpt3_completion_response.choices)
+
+
+def test_run():
+    message_suggestions: list[str] = get_suggestions_for_next_message(MOCK_CHATLOG)
+    
+    assert message_suggestions == [
+        "Bot - I am a web developer.",
+        "Bot - I work in customer service for a company.",
+        "Bot - I am a software engineer.",
+    ]
