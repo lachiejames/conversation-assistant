d34ca5d35c4df6dd73d7971ee6a7053660d1281b
lachiejames <lachie_james@hotmail.com.au>
2022-01-04 02:26:25 +1100

    fix failing tests

--- conversation_assistant/test/test_gpt3.py
+++ conversation_assistant/test/test_gpt3.py
@@ -1,12 +1,6 @@
 from conversation_assistant.gpt3 import fetch_completetion
-from conversation_assistant.models import GPT3CompletionResponse, GPT3Params
-from conversation_assistant.test.mocks import MOCK_PROMPT
-
-MOCK_GPT3_PARAMS: GPT3Params = {
-    "randomness": 0.7,
-    "num_results": 3,
-    "max_length": 50,
-}
+from conversation_assistant.models import GPT3CompletionResponse
+from conversation_assistant.test.mocks import MOCK_GPT3_PARAMS, MOCK_PROMPT
 
 
 def test_fetch_completetion_returns_3_choices():
