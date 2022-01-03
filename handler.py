861bc5ba1054a390fc42e0e72186434e0339f546
lachiejames <lachie_james@hotmail.com.au>
2022-01-03 13:48:22 +1100

    get lambda working

--- handler.py
+++ handler.py
@@ -1,8 +1,11 @@
 import json
+from dotenv import load_dotenv
 from conversation_assistant.generator import generate_message_suggestions
 
 
 def lambda_handler(event, context):
+    load_dotenv(".env")
+
     return json.dumps(
         {
             "results": generate_message_suggestions(
@@ -12,7 +15,5 @@ def lambda_handler(event, context):
                     {"text": "Yeah good mate.  What have you been up to lately?", "author": "Lachie James"},
                 ]
             ),
-            "event": event,
-            context: context,
         }
     )
