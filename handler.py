57643e8f93ccbf8d4a5a1a2ec3bbccb9fd0b50b9
lachiejames <lachie_james@hotmail.com.au>
2022-01-03 15:07:07 +1100

    allow messages to be generated via lambda event

--- handler.py
+++ handler.py
@@ -1,19 +1,19 @@
 import json
 from dotenv import load_dotenv
 from conversation_assistant.generator import generate_message_suggestions
+from conversation_assistant.models import Message, Suggestion
 
 
 def lambda_handler(event, context):
     load_dotenv(".env")
 
-    return json.dumps(
-        {
-            "results": generate_message_suggestions(
-                [
-                    {"text": "Hey, how are you today?", "author": "Lachie James"},
-                    {"text": "Not too bad mate, how are you?", "author": "Me"},
-                    {"text": "Yeah good mate.  What have you been up to lately?", "author": "Lachie James"},
-                ]
-            ),
-        }
-    )
+    previous_messages: list[Message] = event["previous_messages"]
+    suggestions: list[Suggestion] = generate_message_suggestions(previous_messages)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps({"results": suggestions}),
+    }
