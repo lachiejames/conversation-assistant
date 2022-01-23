972ef7d1fc119b64eb733fa3616b50a0be6d89bd
lachiejames <lachie_james@hotmail.com.au>
2022-01-23 16:05:08 +1100

    add validation tests

--- conversation_assistant/test/test_validate_schema.py
+++ conversation_assistant/test/test_validate_schema.py
@@ -1,16 +1,75 @@
+from typing import Any
+
+import pytest
+from jsonschema import ValidationError
+
 from conversation_assistant.validate_schema import validate_message_suggestions
 
 
-def test_validate_message_suggestions():
-    validate_message_suggestions(
-        {
-            "body": {
-                "previous_messages": [],
-                "gpt3_params": {
-                    "randomness": 0.7,
-                    "num_results": 3,
-                    "max_length": 50,
+def test_validate_message_suggestions__when_event_is_valid__then_succeeds():
+    valid_event: Any = {
+        "body": {
+            "previous_messages": [
+                {
+                    "text": "Oi mate",
+                    "author": "Ben Kerr",
                 },
-            }
+                {
+                    "text": "Yo dude",
+                    "author": "Me",
+                },
+            ],
+            "gpt3_params": {
+                "randomness": 0.7,
+                "num_results": 3,
+                "max_length": 50,
+            },
         }
-    )
+    }
+
+    validate_message_suggestions(valid_event)
+
+
+def test_validate_message_suggestions__when_event_is_valid_and_previous_messages_empty__then_succeeds():
+    valid_event: Any = {
+        "body": {
+            "previous_messages": [],
+            "gpt3_params": {
+                "randomness": 0.7,
+                "num_results": 3,
+                "max_length": 50,
+            },
+        }
+    }
+
+    validate_message_suggestions(valid_event)
+
+
+def test_validate_message_suggestions__when_event_not_wrapped_in_body__then_raises_validation_error():
+    invalid_event: Any = {
+        "previous_messages": [
+            {
+                "text": "Oi mate",
+                "author": "Ben Kerr",
+            },
+            {
+                "text": "Yo dude",
+                "author": "Me",
+            },
+        ],
+        "gpt3_params": {
+            "randomness": 0.7,
+            "num_results": 3,
+            "max_length": 50,
+        },
+    }
+
+    with pytest.raises(ValidationError):
+        validate_message_suggestions(invalid_event)
+
+
+def test_validate_message_suggestions__when_event_is_empty__then_raises_validation_error():
+    invalid_event: Any = {"body": {}}
+
+    with pytest.raises(ValidationError):
+        validate_message_suggestions(invalid_event)
