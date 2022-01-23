0f8aa80f5078946015141d3ef3ef470231ec7787
lachiejames <lachie_james@hotmail.com.au>
2022-01-23 16:17:30 +1100

    extract lambda response logic into CA module

---
+++ conversation_assistant/lambda_responses.py
@@ -0,0 +1,53 @@
+import json
+
+from conversation_assistant.generator import generate_message_suggestions
+from conversation_assistant.models import LambdaEvent, LambdaRequest, Suggestion
+
+
+def respond_with_200(event: LambdaEvent):
+    print("The event is valid.  Generating message suggestions...")
+    suggestions: list[Suggestion] = generate_message_suggestions(event)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps({"results": suggestions}),
+    }
+
+
+def respond_with_400(event: LambdaEvent):
+    print("The event is NOT valid, responding with error")
+
+    return {
+        "statusCode": 400,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps({"error": f"Invalid request body - {event}"}),
+    }
+
+
+def respond_with_500(request: LambdaRequest):
+    print("The event is NOT valid, responding with error")
+
+    return {
+        "statusCode": 500,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps({"error": f"Invalid request - {request}"}),
+    }
+
+
+def lambda_response(request: LambdaRequest):
+    if "body" in request:
+        lambda_event: LambdaEvent = json.loads(request["body"])
+
+        if "previous_messages" in lambda_event and "gpt3_params" in lambda_event:
+            return respond_with_200(lambda_event)
+
+        return respond_with_400(lambda_event)
+
+    return respond_with_500(request)
--- conversation_assistant/validators.py
+++ conversation_assistant/validators.py
@@ -6,7 +6,7 @@ from jsonschema import validate
 
 
 def validate_message_suggestions(obj: Any):
-    path_to_schema: str = os.path.join("schemas", f"generate-message-suggestions.json")
+    path_to_schema: str = os.path.join("schemas", "generate-message-suggestions.json")
 
     with open(path_to_schema, "r", encoding="utf-8") as schema_file:
         schema: Any = json.load(schema_file)
