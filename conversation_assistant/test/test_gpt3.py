836d42d5f565dc08adf253419eb12c852da6758d
lachiejames <lachie_james@hotmail.com.au>
2022-03-16 20:09:41 +1100

    moar tests

--- conversation_assistant/test/test_gpt3.py
+++ conversation_assistant/test/test_gpt3.py
@@ -1,25 +1,34 @@
 from unittest.mock import MagicMock, patch
+from jsonschema import ValidationError
+
+import pytest
 
 from conversation_assistant.models.gpt3_params import GPT3Params
 
 from ..gpt3 import fetch_completion
-from .mocks import MOCK_PROMPT, MOCK_REQUEST
+from .mocks import MOCK_GPT3_COMPLETION_RESPONSE, MOCK_PROMPT, MOCK_REQUEST
 
 
-@patch("conversation_assistant.gpt3.Completion.create")
-def test_fetch_completion__when_all_gpt3_params_passed__then_each_param_mapped_to_api_param(create_completion_spy: MagicMock):
+@patch("conversation_assistant.gpt3.Completion.create", MagicMock(return_value=MOCK_GPT3_COMPLETION_RESPONSE))
+def test_fetch_completion__when_gpt3_request_succeeds__then_returns_response():
     mock_gpt3_params: GPT3Params = MOCK_REQUEST["gpt3_params"]
 
-    fetch_completion(MOCK_PROMPT, mock_gpt3_params)
+    response = fetch_completion(MOCK_PROMPT, mock_gpt3_params)
 
-    call_dict = create_completion_spy.call_args_list[0][1]
+    assert response is MOCK_GPT3_COMPLETION_RESPONSE
 
-    assert call_dict["prompt"] is MOCK_PROMPT
-    assert call_dict["n"] is mock_gpt3_params["n"]
-    assert call_dict["temperature"] is mock_gpt3_params["temperature"]
-    assert call_dict["max_tokens"] is mock_gpt3_params["max_tokens"]
-    assert call_dict["top_p"] is mock_gpt3_params["top_p"]
-    assert call_dict["best_of"] is mock_gpt3_params["best_of"]
-    assert call_dict["frequency_penalty"] is mock_gpt3_params["frequency_penalty"]
-    assert call_dict["presence_penalty"] is mock_gpt3_params["presence_penalty"]
-    assert call_dict["stop"] is mock_gpt3_params["stop"]
+
+@patch("conversation_assistant.gpt3.Completion.create", MagicMock(side_effect=LookupError))
+def test_fetch_completion__when_gpt3_request_fails__then_raises_error():
+    mock_gpt3_params: GPT3Params = MOCK_REQUEST["gpt3_params"]
+
+    with pytest.raises(LookupError):
+        fetch_completion(MOCK_PROMPT, mock_gpt3_params)
+
+
+@patch("conversation_assistant.gpt3.Completion.create", MagicMock(return_value={"this": "will fail"}))
+def test_fetch_completion__when_gpt3_request_succeeds_but_validation_fails__then_raises_validation_error():
+    mock_gpt3_params: GPT3Params = MOCK_REQUEST["gpt3_params"]
+
+    with pytest.raises(ValidationError):
+        fetch_completion(MOCK_PROMPT, mock_gpt3_params)
