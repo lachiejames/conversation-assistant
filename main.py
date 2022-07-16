8fcf7e591aa68a5b770c252dcb55429b968487f1
lachiejames <lachie_james@hotmail.com.au>
2022-07-17 03:38:38 +1000

    build requirements

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import functions_framework  # type: ignore
 from flask import Request
 
 from conversation_assistant import run_generate_suggestions
 from conversation_assistant.models import GenerateSuggestionsResponse
 
 
-@functions_framework.http
 def generate_suggestions(request: Request) -> GenerateSuggestionsResponse:
     return run_generate_suggestions(request.get_json(silent=True))
