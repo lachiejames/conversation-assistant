a42ec78d15ab7ab377607609674c9945f2d98eb9
lachiejames <lachie_james@hotmail.com.au>
2022-07-17 00:27:18 +1000

    use real request for e2e test

--- e2e/test_main.py
+++ e2e/test_main.py
@@ -1,21 +1,31 @@
 import json
 import os
 from typing import Any, List
-import unittest
-import unittest.mock
+
+import flask
+import pytest
 from conversation_assistant.models import GenerateMessageSuggestionsRequest, LambdaResponse, Suggestion
-from conversation_assistant.test.mocks import MOCK_REQUEST, MOCK_SUGGESTIONS
 
 from main import generate_suggestions
+import requests
 
 
-def test_generate_suggestions__when_valid_request_received__then_returns_1_suggestion():
-    path_to_schema: str = os.path.join("events", "tinder.json")
-    with open(path_to_schema, "r", encoding="utf-8") as schema_file:
-        request: GenerateMessageSuggestionsRequest = json.load(schema_file)
+# Create a fake "app" for generating test request contexts.
+@pytest.fixture(scope="module")
+def app() -> flask.Flask:
+    return flask.Flask(__name__)
 
-        response: LambdaResponse = generate_suggestions(request)
-        parsed_response: Any = json.loads(response["body"])
-        suggestions: List[Suggestion] = parsed_response["results"]
 
-        assert len(suggestions) == 1
+def get_path_to_file(filename: str) -> str:
+    return os.path.abspath(os.path.join(__file__, "..", "mock_requests", filename))
+
+
+def test_generate_suggestions__when_valid_request_received__then_returns_1_suggestion(app: flask.Flask):
+    with open(get_path_to_file("tinder.json"), "r", encoding="utf-8") as example_request_file:
+        with app.test_request_context(json=json.load(example_request_file)):
+            response: LambdaResponse = generate_suggestions(flask.request)
+
+            parsed_response: Any = json.loads(response["body"])
+            suggestions: List[Suggestion] = parsed_response["results"]
+
+            assert len(suggestions) == 1
