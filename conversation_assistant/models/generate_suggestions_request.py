29a6f73e1d7143a2b3325a1cb58bbd63cfca2d69
lachiejames <lachie_james@hotmail.com.au>
2022-07-17 03:15:34 +1000

    refactor types

---
+++ conversation_assistant/models/generate_suggestions_request.py
@@ -0,0 +1,57 @@
+from typing_extensions import TypedDict
+
+from .message import Message
+
+ConversationParams = TypedDict(
+    "ConversationParams",
+    {
+        "their_name": str,
+        "their_relationship_to_me": str,
+        "tone_of_chat": str,
+    },
+)
+
+ProfileParams = TypedDict(
+    "ProfileParams",
+    {
+        "name": str,
+        "age": str,
+        "pronouns": str,
+        "location": str,
+        "occupation": str,
+        "hobbies": str,
+        "self_description": str,
+    },
+)
+
+GPT3Params = TypedDict(
+    "GPT3Params",
+    {
+        "engine": str,
+        "n": int,
+        "temperature": float,
+        "max_tokens": int,
+        "top_p": float,
+        "best_of": int,
+        "frequency_penalty": float,
+        "presence_penalty": float,
+    },
+)
+
+
+Settings = TypedDict(
+    "Settings",
+    {
+        "conversation_params": ConversationParams,
+        "profile_params": ProfileParams,
+        "gpt3_params": GPT3Params,
+    },
+)
+
+GenerateSuggestionsRequest = TypedDict(
+    "GenerateSuggestionsRequest",
+    {
+        "previous_messages": list[Message],
+        "settings": Settings,
+    },
+)
