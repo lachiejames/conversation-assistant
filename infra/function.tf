dbc46399b87a66edcaabef13a278fca834f9c956
lachiejames <lachie_james@hotmail.com.au>
2022-07-23 21:57:51 +1000

    build function for deployment

--- infra/function.tf
+++ infra/function.tf
@@ -3,10 +3,17 @@ resource "google_storage_bucket" "bucket" {
   location = "US"
 }
 
-resource "google_storage_bucket_object" "archive" {
-  name   = "${var.function_name}.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "../main.py"
+data "archive_file" "archive" {
+  type        = "zip"
+  source  = "../"
+  output_path = "/tmp/function.zip"
+}
+
+resource "google_storage_bucket_object" "object" {
+  name         = "${var.function_name}.zip"
+  content_type = "application/zip"
+  source       = data.archive_file.archive.output_path
+  bucket       = google_storage_bucket.bucket.name
 }
 
 resource "google_cloudfunctions_function" "function" {
@@ -14,28 +21,11 @@ resource "google_cloudfunctions_function" "function" {
   description = "Generates message suggestions for a user to respond with based upon their conversation history, and some context parameters"
   runtime     = "python39"
 
-  available_memory_mb          = 128
+  available_memory_mb          = 512
   source_archive_bucket        = google_storage_bucket.bucket.name
-  source_archive_object        = google_storage_bucket_object.archive.name
+  source_archive_object        = google_storage_bucket_object.object.name
   trigger_http                 = true
   https_trigger_security_level = "SECURE_ALWAYS"
   timeout                      = 60
   entry_point                  = "generate_suggestions"
-  # labels = {
-  #   my-label = "my-label-value"
-  # }
-
-  # environment_variables = {
-  #   MY_ENV_VAR = "my-env-var-value"
-  # }
 }
-
-# # IAM entry for a single user to invoke the function
-# resource "google_cloudfunctions_function_iam_member" "invoker" {
-#   project        = google_cloudfunctions_function.function.project
-#   region         = google_cloudfunctions_function.function.region
-#   cloud_function = google_cloudfunctions_function.function.name
-
-#   role   = "roles/cloudfunctions.invoker"
-#   member = "user:myFunctionInvoker@example.com"
-# }
