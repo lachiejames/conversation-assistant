6dfc10789af08737ba8b76e56ff76c3772d5d253
lachiejames <lachie_james@hotmail.com.au>
2022-07-23 23:19:45 +1000

    remove enable opts

--- infra/enable.tf
+++ infra/enable.tf
@@ -2,25 +2,16 @@
 resource "google_project_service" "cloudfunctions" {
   project = "${var.project}-${var.environment}"
   service = "cloudfunctions.googleapis.com"
-
-  disable_dependent_services = true
-  disable_on_destroy         = false
 }
 
 # Enable Cloud Build API
 resource "google_project_service" "cloudbuild" {
   project = "${var.project}-${var.environment}"
   service = "cloudbuild.googleapis.com"
-
-  disable_dependent_services = true
-  disable_on_destroy         = false
 }
 
 # Enable Cloud Storage API
 resource "google_project_service" "storage" {
   project = "${var.project}-${var.environment}"
   service = "storage.googleapis.com"
-
-  disable_dependent_services = true
-  disable_on_destroy         = false
 }
