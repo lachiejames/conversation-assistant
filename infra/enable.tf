45f76bc34c6d95c1c66f8e0e2fb45d22286687fa
lachiejames <lachie_james@hotmail.com.au>
2022-07-23 23:16:42 +1000

    fix api enabling

--- infra/enable.tf
+++ infra/enable.tf
@@ -1,6 +1,6 @@
 # Enable Cloud Functions API
 resource "google_project_service" "cloudfunctions" {
-  project = var.project
+  project = "${var.project}-${var.environment}"
   service = "cloudfunctions.googleapis.com"
 
   disable_dependent_services = true
@@ -9,7 +9,7 @@ resource "google_project_service" "cloudfunctions" {
 
 # Enable Cloud Build API
 resource "google_project_service" "cloudbuild" {
-  project = var.project
+  project = "${var.project}-${var.environment}"
   service = "cloudbuild.googleapis.com"
 
   disable_dependent_services = true
@@ -18,7 +18,7 @@ resource "google_project_service" "cloudbuild" {
 
 # Enable Cloud Storage API
 resource "google_project_service" "storage" {
-  project = var.project
+  project = "${var.project}-${var.environment}"
   service = "storage.googleapis.com"
 
   disable_dependent_services = true
