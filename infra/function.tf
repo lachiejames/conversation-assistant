75e0a04472264df663ed2caf50bb5c7f81586299
lachiejames <lachie_james@hotmail.com.au>
2022-07-23 22:12:17 +1000

    add CF invoker roles

--- infra/function.tf
+++ infra/function.tf
@@ -3,16 +3,17 @@ resource "google_storage_bucket" "bucket" {
   location = "US"
 }
 
-data "archive_file" "archive" {
+# Compress source code
+data "archive_file" "source" {
   type        = "zip"
-  source  = "../"
+  source_dir  = "../conversation_assistant"
   output_path = "/tmp/function.zip"
 }
 
 resource "google_storage_bucket_object" "object" {
   name         = "${var.function_name}.zip"
   content_type = "application/zip"
-  source       = data.archive_file.archive.output_path
+  source       = data.archive_file.source.output_path
   bucket       = google_storage_bucket.bucket.name
 }
 
@@ -28,4 +29,14 @@ resource "google_cloudfunctions_function" "function" {
   https_trigger_security_level = "SECURE_ALWAYS"
   timeout                      = 60
   entry_point                  = "generate_suggestions"
+}
+
+# Create IAM entry so all users can invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
