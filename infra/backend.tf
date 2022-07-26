2c42eb13f9f53309f3386f18829f5a0347d31637
lachiejames <lachie_james@hotmail.com.au>
2022-07-26 22:41:39 +1000

    get it working

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,4 +1,6 @@
 terraform {
+  # NOTE: You will need to comment this out the first time you run `terraform init` on a new project so 
+  # that the TF state bucket is created first
   backend "gcs" {
     bucket = "conversation-assistant-dev-bucket-tfstate"
     prefix = "terraform/state"
