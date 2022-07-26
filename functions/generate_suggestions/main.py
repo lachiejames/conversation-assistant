0855a318e3c584639784d4224b0fbaf2a9b5ae17
lachiejames <lachie_james@hotmail.com.au>
2022-07-27 03:14:56 +1000

    implement functions framework

--- functions/generate_suggestions/main.py
+++ functions/generate_suggestions/main.py
@@ -4,7 +4,9 @@ from flask import Request, Response
 
 from src import run_generate_suggestions
 
+import functions_framework
 
+@functions_framework.http
 def generate_suggestions(request: Request) -> Response:
     request_body: Union[Any, None] = request.get_json(silent=True)
     return run_generate_suggestions(request_body)
