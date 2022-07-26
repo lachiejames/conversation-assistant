f7bbcc80d9c03a7cb9b6047d4b95cc0d9a41baff
lachiejames <lachie_james@hotmail.com.au>
2022-07-26 23:20:59 +1000

    update branches

--- infra/function.tf
+++ infra/function.tf
@@ -40,12 +40,12 @@ resource "google_cloudfunctions_function" "function" {
 }
 
 
-# # Create IAM entry so all users can invoke the function
-# resource "google_cloudfunctions_function_iam_member" "invoker" {
-#   project        = google_cloudfunctions_function.function.project
-#   region         = google_cloudfunctions_function.function.region
-#   cloud_function = google_cloudfunctions_function.function.name
+# Create IAM entry so all users can invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
 
-#   role   = "roles/cloudfunctions.invoker"
-#   member = "allUsers"
-# }
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
