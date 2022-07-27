ecc256957e2e482eaea4044a954a99e938beed76
lachiejames <lachie_james@hotmail.com.au>
2022-07-27 17:20:27 +1000

    comment out steps

--- infra/firebase.tf
+++ infra/firebase.tf
@@ -1,11 +1,11 @@
-resource "google_project" "default" {
-  provider = google-beta
+# resource "google_project" "default" {
+#   provider = google-beta
 
-  project_id = "${local.project}-${var.environment}"
-  name       = "${local.project}-${var.environment}"
-}
+#   project_id = "${local.project}-${var.environment}"
+#   name       = "${local.project}-${var.environment}"
+# }
 
-resource "google_firebase_project" "default" {
-  provider = google-beta
-  project  = google_project.default.project_id
-}
+# resource "google_firebase_project" "default" {
+#   provider = google-beta
+#   project  = "${local.project}-${var.environment}"
+# }
