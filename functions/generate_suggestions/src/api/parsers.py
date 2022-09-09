478dc4b30bd6d32a22601ae2ff3bcd1b30d81e88
lachiejames <lachie_james@hotmail.com.au>
2022-09-10 01:10:26 +1000

    migrate to prompt templates

--- functions/generate_suggestions/src/api/parsers.py
+++ functions/generate_suggestions/src/api/parsers.py
@@ -1,47 +1,33 @@
 from ..models import (
     GenerateSuggestionsRequest,
     GPT3CompletionResponse,
-    Message,
     Suggestion,
 )
 from .translate import DEFAULT_LANG, UNDEFINED_LANG, translate_text
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 
 def generate_prompt(request: GenerateSuggestionsRequest, input_lang: str) -> str:
-    my_name: str = request["settings"]["profile_params"]["name"]
-    my_age: str = request["settings"]["profile_params"]["age"]
-    my_pronouns: str = request["settings"]["profile_params"]["pronouns"]
-    my_location: str = request["settings"]["profile_params"]["location"]
-    my_occupation: str = request["settings"]["profile_params"]["occupation"]
-    my_hobbies: str = request["settings"]["profile_params"]["hobbies"]
-    my_self_description: str = request["settings"]["profile_params"]["self_description"]
+    env = Environment(
+        loader=PackageLoader(package_name="src"),
+        autoescape=select_autoescape(),
+        keep_trailing_newline=True,
+    )
+    template = env.get_template("nothing_missing.md")
 
-    their_name: str = request["settings"]["conversation_params"]["their_name"]
-    their_relationship_to_me: str = request["settings"]["conversation_params"]["their_relationship_to_me"]
-    tone_of_chat: str = request["settings"]["conversation_params"]["tone_of_chat"]
-
-    previous_messages: list[Message] = request["previous_messages"]
-
-    prompt = f"""
-The following is a conversation between {my_name} and {their_name}, who is {my_name}'s {their_relationship_to_me}.
-{my_name} is a {my_age} year old {my_occupation} who lives in {my_location}.
-{my_name}'s pronouns are {my_pronouns}.
-{my_name}'s favourite hobbies include {my_hobbies}.
-{my_name} can be described as {my_self_description}.
-The tone of this conversation is {tone_of_chat}.
-
-"""
-
-    if input_lang != DEFAULT_LANG and input_lang != UNDEFINED_LANG:
-        prompt = translate_text(prompt, input_lang)
-
-    for message in previous_messages:
-        prompt += f"{message['author']}: {message['text']}\n"
-
-    # Generate the next message from the perspective of the 'me' (the user)
-    prompt += f"{my_name}:"
-
-    return prompt
+    return template.render(
+        my_name=request["settings"]["profile_params"]["name"],
+        my_age=request["settings"]["profile_params"]["age"],
+        my_pronouns=request["settings"]["profile_params"]["pronouns"],
+        my_location=request["settings"]["profile_params"]["location"],
+        my_occupation=request["settings"]["profile_params"]["occupation"],
+        my_hobbies=request["settings"]["profile_params"]["hobbies"],
+        my_self_description=request["settings"]["profile_params"]["self_description"],
+        their_name=request["settings"]["conversation_params"]["their_name"],
+        their_relationship_to_me=request["settings"]["conversation_params"]["their_relationship_to_me"],
+        tone_of_chat=request["settings"]["conversation_params"]["tone_of_chat"],
+        previous_messages=request["previous_messages"],
+    )
 
 
 def map_completion_response_to_suggestions(response: GPT3CompletionResponse) -> list[Suggestion]:
---
+++ functions/generate_suggestions/src/templates/nothing_missing.md
@@ -0,0 +1,12 @@
+The following is a conversation between {{ my_name }} and {{ their_name }}, who is {{ my_name }}'s {{ their_relationship_to_me }}.
+{{ my_name }} is a {{ my_age }} year old {{ my_occupation }} who lives in {{ my_location }}.
+{{ my_name }}'s pronouns are {{ my_pronouns }}.
+{{ my_name }}'s favourite hobbies include {{ my_hobbies }}.
+{{ my_name }} can be described as {{ my_self_description }}.
+The tone of this conversation is {{ tone_of_chat }}.
+
+{% for message in previous_messages %}
+{{ message['author'] }}: {{ message['text'] }}
+{% endfor %}
+
+{{ my_name }}:
