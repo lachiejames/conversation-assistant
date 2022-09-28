abd746a4edfbb50b8dd6e3710789905088d24ed5
lachiejames <lachie_james@hotmail.com.au>
2022-09-29 04:44:40 +1000

    add types

---
+++ functions/transcribe/src/models/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable=unused-import
+from .transcribe_request import (
+    ConversationParams,
+    TranscribeRequest,
+    GPT3Params,
+    ProfileParams,
+)
+from .transcribe_response import TranscribeResponse
+from .message import Message
