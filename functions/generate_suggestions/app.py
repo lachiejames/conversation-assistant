06ba1c6d716d1353b7a8fa7c46169febd0bac1f4
lachiejames <lachie_james@hotmail.com.au>
2022-09-28 14:51:39 +1000

    now serve html stream with python

--- functions/generate_suggestions/app.py
+++ functions/generate_suggestions/app.py
@@ -1,10 +1,20 @@
-import flask
+# import flask
 
-import main
+# import main
 
-app = flask.Flask(__name__)
+# app = flask.Flask(__name__)
 
 
-@app.route("/generate-suggestions-function", methods=["POST"])
-def generate() -> flask.Response:
-    return main.generate_suggestions(flask.request)
+# @app.route("/generate-suggestions-function", methods=["POST"])
+# def generate() -> flask.Response:
+#     return main.generate_suggestions(flask.request)
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run()
