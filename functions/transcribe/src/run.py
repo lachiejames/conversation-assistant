e53a1f126fb40c275fd59b7ad14d8106cdf0ed3e
lachiejames <lachie_james@hotmail.com.au>
2022-09-29 02:26:43 +1000

    refactorino

--- functions/transcribe/src/run.py
+++ functions/transcribe/src/run.py
@@ -1,53 +1,54 @@
 import os
 from scipy.io import wavfile
-from google.cloud import speech
+from google.cloud.speech import RecognitionConfig, RecognitionAudio, SpeechClient
 import json
 from typing import Any, Union
 
 from flask import Response
 
+DEFAULT_SAMPLE_RATE = 44100
+DEFAULT_LANG = "en-US"
+
 
 def run_transcribe(request_body: Union[Any, None]) -> Response:
-    # file_path = os.path.abspath(os.path.join(__file__, "..", "..", "assets", "test2.wav"))
-    transcription = transcribe_input(request_body)
-
     return Response(
-        response=json.dumps({"body": transcription}),
         status=200,
         headers={
             "Content-Type": "application/json",
         },
+        response=json.dumps(
+            {
+                "body": transcribe_input(request_body),
+            }
+        ),
     )
+
 
 def transcribe_input(input: str) -> str:
-    client = speech.SpeechClient()
-    audio = speech.RecognitionAudio(content=input)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
-        language_code="en-US",
+    client = SpeechClient()
+    audio = RecognitionAudio(content=input)
+    config = RecognitionConfig(
+        encoding=RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=DEFAULT_SAMPLE_RATE,
+        language_code=DEFAULT_LANG,
     )
-    operation = client.long_running_recognize(config=config, audio=audio)
-    print("Waiting for operation to complete...")
-    response = operation.result(timeout=90)
+    response = client.recognize(config=config, audio=audio)
 
     return response.results[0].alternatives[0].transcript
+
 
 def transcribe_file(speech_file: str) -> str:
     samplerate, _ = wavfile.read(speech_file)
-    client = speech.SpeechClient()
+    client = SpeechClient()
     with open(speech_file, "rb") as audio_file:
         content = audio_file.read()
 
-    audio = speech.RecognitionAudio(content=content)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    audio = RecognitionAudio(content=content)
+    config = RecognitionConfig(
+        encoding=RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=samplerate,
-        language_code="en-US",
+        language_code=DEFAULT_LANG,
     )
-    operation = client.long_running_recognize(config=config, audio=audio)
-    print("Waiting for operation to complete...")
-    response = operation.result(timeout=90)
+    response = client.recognize(config=config, audio=audio)
 
     return response.results[0].alternatives[0].transcript
-
