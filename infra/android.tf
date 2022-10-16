2684f1cac7d9013db36eb8f572af8d98aa2c38f4
lachiejames <lachie_james@hotmail.com.au>
2022-10-17 04:29:30 +1100

    remove tf change

--- infra/android.tf
+++
@@ -1,5 +0,0 @@
-# Stores resources for the Conversation Assistant Android app
-resource "google_storage_bucket" "bucket-android-assets" {
-  name     = "${local.project}-${var.environment}-bucket-android-assets"
-  location = "US"
-}
