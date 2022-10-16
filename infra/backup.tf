8e77ab73b9491901c94e519abf3ef5fa74c1799e
lachiejames <lachie_james@hotmail.com.au>
2022-10-17 04:55:48 +1100

    replace - with _ in tf

--- infra/backup.tf
+++ infra/backup.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "bucket-firestore-backup" {
+resource "google_storage_bucket" "bucket_firestore_backup" {
   name     = "${local.project}-${var.environment}-bucket-firestore-backup"
   location = "US"
 }
