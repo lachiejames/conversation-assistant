cf9b004144ee6e05dc41dbd8cc403dfcd3cc1310
lachiejames <lachie_james@hotmail.com.au>
2022-10-18 04:33:50 +1100

    try using a for loop

--- infra/android.tf
+++ infra/android.tf
@@ -1,16 +1,32 @@
+locals {
+  apps      = ["Gmail", "Messenger", "Outlook", "Slack", "SMS", "WhatsApp"]
+  tutorials = ["Simple", "Advanced"]
+}
+
 resource "google_storage_bucket" "bucket_android_assets" {
   name     = "${local.project}-${var.environment}-bucket-android-assets"
   location = "US"
-}
-
-resource "google_storage_bucket_object" "simple_gmail" {
-  name   = "simple_gmail.gif"
-  source = "../assets/generate_suggestions/advanced-gmail.gif"
-  bucket = google_storage_bucket.bucket_android_assets.name
 }
 
 resource "google_storage_default_object_access_control" "public" {
   bucket = google_storage_bucket.bucket_android_assets.name
   role   = "READER"
   entity = "allUsers"
+}
+
+# TODO: Figure out how to convert all of this to a for loop
+resource "google_storage_bucket_object" "android_assets" {
+  for_each = {
+    for tutorial in local.tutorials : "${tutorials}-android-assets" => {
+      for app in local.apps : "${app}-android" => {
+        bucket = google_storage_bucket.bucket_android_assets.name
+        name   = "${tutorial}_${app}.gif"
+        source = "../assets/generate_suggestions/${tutorial}_${app}.gif"
+      }
+    }
+  }
+
+  bucket = each.value.bucket
+  name   = each.value.name
+  source = each.value.source
 }
