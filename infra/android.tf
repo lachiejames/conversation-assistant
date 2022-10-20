b59a5c5d4b049616c558a9cf0a9ed9e01a2a108c
lachiejames <lachie_james@hotmail.com.au>
2022-10-20 20:57:48 +1100

    try use each.key

--- infra/android.tf
+++ infra/android.tf
@@ -10,10 +10,10 @@ resource "google_storage_bucket_iam_binding" "binding" {
 }
 
 resource "google_storage_bucket_object" "android_assets" {
-  for_each     = fileset("../assets/generate_suggestions/", "*")
+  for_each = fileset("../assets/generate_suggestions/", "*.gif")
 
   name         = each.value
   content_type = "image/gif"
-  source       = "../assets/generate_suggestions/${each.value}"
+  source       = "${path.module}/../assets/generate_suggestions/${each.key}"
   bucket       = google_storage_bucket.bucket_android_assets.name
 }
